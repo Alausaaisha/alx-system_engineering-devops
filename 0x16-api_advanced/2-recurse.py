c427c9e9015305f6b63135a8e66d5da115566ac7
Alausa Aishat <qtaisha.ao@gmail.com>
2024-08-16 03:11:38 +0100

    a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""queries Reddit API to get all hot articles of a subreddit"""
+
+
+import requests as req
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    """get all the hot articles of a subreddit"""
+    li = []
+    base = 'https://www.reddit.com'
+    header = {'User-agent': 'aisha_alausa'}
+    respons = req.get('{}/r/{}/hot.json?after={}&limit=200'
+                      .format(base, subreddit, after),
+                      headers=header, allow_redirects=False)
+    if respons.status_code != 200:
+        return None
+    posts = respons.json().get('data').get('children')
+    _next = respons.json().get('data').get('after')
+    for post in posts:
+        hot_list.append(post.get('data').get('title'))
+
+    if _next is not None:
+        li.append(hot_list)
+        recurse(subreddit, hot_list, _next)
+    return hot_list
